5dbaa523efa5efdec02acaa75ecfe00ddeb41bbd
Marat Abrarov <abrarov@gmail.com>
2016-05-21 15:57:52 +0300

    Initial example of usage of LibVLC.

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <memory>
+#include <chrono>
+#include <thread>
+#include <vlc/vlc.h>
+
+int main(int argc, char* argv[])
+{
+  typedef std::unique_ptr<libvlc_instance_t, void (*)(libvlc_instance_t*)>
+      libvlc_instance_ptr;
+  typedef std::unique_ptr<libvlc_media_t, void (*)(libvlc_media_t*)>
+      libvlc_mdeia_ptr;
+  typedef std::unique_ptr<libvlc_media_player_t,
+      void (*)(libvlc_media_player_t*)> libvlc_media_player_ptr;
+
+  // Load the VLC engine
+  libvlc_instance_ptr libvlc_instance(libvlc_new(0, nullptr), libvlc_release);
+
+  // Create a new item
+  libvlc_mdeia_ptr media(libvlc_media_new_path(libvlc_instance.get(),
+      "/home/marat/Видео/Winnie-the-Pooh.avi"), libvlc_media_release);
+
+  // Create a media player playing environment
+  libvlc_media_player_ptr media_player
+      (libvlc_media_player_new_from_media(media.get()),
+          libvlc_media_player_release);
+
+  // Media is not needed anymore
+  media.reset();
+
+  // Start playing
+  libvlc_media_player_play(media_player.get());
+
+  // Let it play a bit
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+
+  // Stop playing
+  libvlc_media_player_stop(media_player.get());
+
+  return EXIT_SUCCESS;
+}
